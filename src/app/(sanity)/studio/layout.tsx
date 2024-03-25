9d57287a19ac22d4919197c3577b51656c70a06e
Felipe Trevisan <felipejs@gmail.com>
2024-03-25 17:22:06 -0300

    feat: add sanity studio page

---
+++ src/app/(sanity)/studio/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+
+import "../../globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+  variable: "--inter",
+});
+
+export const metadata = {
+  title: {
+    template: "%s | Pardini Cidadania",
+    absolute: "Pardini Cidadania",
+  },
+};
+
+export default function StudioLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={`${inter.className}`} suppressHydrationWarning>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
