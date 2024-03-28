1597d4db50d40ae0947bcc951fa833a1cd43aa84
Felipe Trevisan <felipejs@gmail.com>
2024-03-28 01:06:29 -0300

    fix: fix image in faq section

--- src/components/sections/faq/index.tsx
+++ src/components/sections/faq/index.tsx
@@ -6,19 +6,19 @@ import { Questions } from "./questions";
 export async function Faq() {
   return (
     <div className="h-full w-screen flex my-20" data-section="faq">
-      <div className="container flex flex-col md:flex-row lg:flex-row items-center justify-start gap-12">
-        <div className="w-full h-full lg:w-1/2 hidden md:flex justify-center items-center">
-          <div className="flex justify-center max-w-[70%] lg:max-w-full w-full h-full relative">
+      <div className="container flex flex-col md:grid md:grid-cols-2 items-center justify-start gap-12">
+        <div className="md:w-full md:h-full md:flex md:justify-center md:items-center hidden md:relative">
+          <div className="flex justify-center w-full h-full md:max-h-[440px] lg:max-h-[740px] relative md:absolute">
             <Image
               src="/assets/passport.png"
               alt=""
               fill
-              className="w-full h-full object-cover"
+              className="object-cover"
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
           </div>
         </div>
-        <div className="w-full lg:w-1/2 flex flex-col gap-5">
+        <div className="w-full flex flex-col gap-5">
           <App.Title>Perguntas frequentes</App.Title>
           <App.Subtitle>
             Veja algumas perguntas frequentes <br /> de{" "}
