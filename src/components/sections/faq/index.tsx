767850a5892f07d5ac09e1c2afc4b084b7e97a38
Felipe Trevisan <felipejs@gmail.com>
2024-11-17 20:42:44 -0300

    fix: fix grid on large screen

--- src/components/sections/faq/index.tsx
+++ src/components/sections/faq/index.tsx
@@ -5,7 +5,7 @@ export async function Faq() {
   return (
     <div className="container h-full w-screen flex my-20 relative" data-section="faq">
       <div className="container flex flex-col items-center md:items-end justify-center">
-        <div className="grid grid-cols-[80vw] lg:grid-cols-[repeat(2,minmax(0,22vw))] xl:grid-cols-[repeat(2,minmax(0,20vw))] lg:col-start-1 lg:col-end-2">
+        <div className="grid grid-cols-[80vw] lg:grid-cols-[repeat(2,minmax(0,22vw))] xl:grid-cols-[repeat(2,minmax(0,15vw))] lg:col-start-1 lg:col-end-2">
           <div className="lg:col-span-2">
             <App.Title>Perguntas frequentes</App.Title>
             <App.Subtitle>
